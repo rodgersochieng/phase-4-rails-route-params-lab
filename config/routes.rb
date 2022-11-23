35d69d2f8f60355e34faa8e3526966c355803d93
rodgersochieng <sirrodgers21@gmail.com>
2022-11-23 22:12:27 +0300

    completed lab

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  get '/studends/:id', to: 'students#show'
+  # get '/students/1', to: 'students#show'
+  # get '/students/2', to: 'students#show'
+
+
+  
+
 end
